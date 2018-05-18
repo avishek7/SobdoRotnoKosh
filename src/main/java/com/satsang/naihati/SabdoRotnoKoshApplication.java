395f2afcbd4d0a3245f8667569446b4a374bff56
Pal Chowdhury <684557@cognizant.com>
2018-05-18 17:44:07 +0530

    initial commit

---
+++ src/main/java/com/satsang/naihati/SabdoRotnoKoshApplication.java
@@ -0,0 +1,12 @@
+package com.satsang.naihati;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SabdoRotnoKoshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SabdoRotnoKoshApplication.class, args);
+	}
+}
